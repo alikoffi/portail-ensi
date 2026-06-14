f4284bad0d476251cf249281d45690a99c2503d0
KOFFI ANSELME ALI <ali.koffi@eburtis.ci>
2026-06-14 23:22:12 +0000

    Pour déployer

--- backend/src/main/resources/flyway/schema/V0.0.5__maj_admin.sql
+++
@@ -1,6 +0,0 @@
--- Mise a jour des identifiants du compte administrateur
--- Mot de passe : ensi@2026 (hash bcrypt)
-UPDATE utilisateur
-SET password_hash = '$2a$10$mbCKed8QgQzs54sjANjgUezPLxIsbWqyFck2K5j5LSi5tTC5wjJ8e',
-    email = 'ably84@gmail.com'
-WHERE username = 'admin';
